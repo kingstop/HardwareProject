19361a4a46da3758903b3beabd92e4ee975d8a8b
kingstop <kingstop@sina.com>
2017-03-28 17:40:41 +0800

    add

--- server/server/server/message/gen.bat
+++
@@ -1,5 +0,0 @@
-﻿protogen.exe -i:common.proto -o:..\..\..\client\pushmole\Assets\mole_net\message\common_msg.cs 
-protogen.exe -i:crashmo.proto -o:..\..\..\client\pushmole\Assets\mole_net\message\crashmo_msg.cs
-protogen.exe -i:login.proto -o:..\..\..\client\pushmole\Assets\mole_net\message\login_msg.cs
-protogen.exe -i:common.proto -o:..\..\..\client\pushmole\Assets\mole_net\message\common_msg.cs 
-pause
